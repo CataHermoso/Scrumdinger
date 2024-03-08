3786cb22a34b610a9cc87b10d476d867054fb86d
Catalina Hermoso <93346821+CataHermoso@users.noreply.github.com>
2024-03-08 00:12:05 -0300

    Initial Commit

---
+++ Scrumdinger/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Scrumdinger
+//
+//  Created by Catalina Hermoso on 08/03/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by Catalina Hermoso on 08/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
