ddd21933ee529fb776f9ddf2f945d2010be50fc2
Catalina Hermoso <93346821+CataHermoso@users.noreply.github.com>
2024-03-10 13:34:20 -0300

    creating a daily scum model with dummy data

---
+++ Scrumdinger/Models/DailyScrum.swift
@@ -0,0 +1,24 @@
+//
+//  DailyScrum.swift
+//  Scrumdinger
+//
+//  Created by Catalina Hermoso on 10/03/2024.
+//
+
+import Foundation
+
+struct DailyScrum {
+    var title: String
+    var attendees: [String]
+    var lengthInMinutes: Int
+    var theme: Theme
+}
+
+extension DailyScrum {
+    static let sampleData: [DailyScrum] =
+    [
+        DailyScrum(title: "Design", attendees: ["Ariel", "Martin", "Simon", "Marta"], lengthInMinutes: 10, theme: .yellow),
+        DailyScrum(title: "App Dev", attendees: ["Bruno", "Elian", "Chachi", "Catalina", "Dim"], lengthInMinutes: 5, theme: .orange),
+        DailyScrum(title: "Web Dev", attendees: ["Tomas", "Iara", "Andy", "Camila", "Sofia", "Carlos", "Axel", "Alex"], lengthInMinutes: 5, theme: .poppy)
+    ]
+}
