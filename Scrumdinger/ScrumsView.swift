a4e74ab943f3e01443347a29eafa02b3335112e1
Catalina Hermoso <93346821+CataHermoso@users.noreply.github.com>
2024-03-10 15:05:42 -0300

    setting up navigation

--- Scrumdinger/ScrumsView.swift
+++ Scrumdinger/ScrumsView.swift
@@ -10,9 +10,20 @@ import SwiftUI
 struct ScrumsView: View {
     let scrums: [DailyScrum]
     var body: some View {
-        List(scrums) { scrum in //this list needs to return a view
-            CardView(scrum:scrum)
+        NavigationStack { //set up the navigation
+            List(scrums) { scrum in //this list needs to return a view
+                NavigationLink(destination: Text(scrum.title)){ //presents a sigle view in the nav hierarchy when a user interacts with the element. each row pushes to an ind destination
+                    CardView(scrum:scrum)
+                }
                 .listRowBackground(scrum.theme.mainColor)
+            }
+            .navigationTitle("Daily Scrums")
+            .toolbar {
+                Button(action: {}) {
+                    Image(systemName: "plus")
+                }
+                .accessibilityLabel("New Scrum")
+            }
         }
     }
 }
